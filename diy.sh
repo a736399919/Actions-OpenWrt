8c4a753f1b8ebd7333c3cfc9157c6716d00a00a6
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-03-31 20:50:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,11 +18,15 @@ sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #添加自定义插件
+git clone https://github.com/coolsnowwolf/lede
+cp -rf lede/package/lean package
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #添加Lienol的插件包
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+#添加自己repo的插件的软连接
+ln -s ../../luci-theme-argon2.1 ./package/
 #修改banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../newifi-y1-banner package/base-files/files/etc/banner
